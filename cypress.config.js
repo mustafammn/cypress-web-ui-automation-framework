8f57115e4527099305522e7e4beee9820addfc10
mustafanooristani <mm.nooristani1@gmail.com>
2025-11-26 14:13:16 -0800

    Enable Video recording and failure screenshot

--- cypress.config.js
+++ cypress.config.js
@@ -5,14 +5,20 @@ module.exports = defineConfig({
     baseUrl: "https://www.saucedemo.com",
     specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
+
+    // Turn ON video + screenshot
+    video: true,
+    screenshotOnRunFailure: true,
     screenshotsFolder: "cypress/screenshots",
     videosFolder: "cypress/videos",
+
     viewportWidth: 1280,
     viewportHeight: 720,
+
     setupNodeEvents(on, config) {
-      // no extra plugins – super stable
       return config;
     },
   },
+
   hideXHR: true,
 });
